f12c351ce4459c3782fb48f83f61a6256bad55bd
Bakhos Ibrahim-Rachwan <bakhos.rachwan@gmail.com>
2021-03-17 00:36:18 +0200

    Feat: fetch and display

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let questionArray = [];
 let answerArray = [];
 let val ={};
 let i = 0;
+let finalScore = 0;
 let amount = 0;
 let URLs = "https://opentdb.com/api.php?amount=10&category=21";
 
@@ -21,33 +22,48 @@ const questions = document.querySelector(".questions-page")
 const question = document.querySelector(".question")
 const answers = document.querySelector(".answers")
 const next = document.querySelector(".next")
+const score = document.querySelector(".score")
 const render = () => {
-    // i<parseInt(amount)? null: window.location = "score.html"
-    let q = questionArray[i].question
-    question.innerHTML = q
-    let correct = questionArray[i].correct_answer
-    let incorrect = questionArray[i].incorrect_answers
-    answerArray.push(correct)
-    answerArray.push(...incorrect)
-    const random = answerArray.sort(() => Math.random() - 0.5)
-    
-    for(let x =0; x<answerArray.length; x++ ){
-        let buttonKey = document.createElement('button')
-        buttonKey.classList.add('button'+x)
-        buttonKey.innerHTML = random[x]
-        answers.appendChild(buttonKey)
-      
-        buttonKey.addEventListener("click", function(){
-            if(buttonKey.innerText === correct){    
-                buttonKey.classList.add('active')
-                // score++
-            }
+    console.log(i)
+    // i<parseInt(amount)? null: score.classList.add("on") 
+    if(i< parseInt(amount)){
+        
+        console.log("if")
+        let q = questionArray[i].question
+        question.innerHTML = q
+        let correct = questionArray[i].correct_answer
+        let incorrect = questionArray[i].incorrect_answers
+        answerArray.push(correct)
+        answerArray.push(...incorrect)
+        console.log(answerArray)
+        const random = answerArray.sort(() => Math.random() - 0.5)
+        
+        for(let x =0; x<answerArray.length; x++ ){
+            let buttonKey = document.createElement('button')
+            buttonKey.classList.add('button'+x)
+            buttonKey.innerHTML = random[x]
+            answers.appendChild(buttonKey)
+            
+            buttonKey.addEventListener("click", function(){
+                if(buttonKey.innerText === correct){    
+                    buttonKey.classList.add('active')
+                    finalScore++
+                }
             else buttonKey.classList.add('inactive')
             next.classList.remove("off")
         })
     }
     answerArray.length = 0;
     i<10? i++: null
+    }
+    else{
+        console.log("else")
+        document.querySelector(".score-val").innerHTML+= finalScore
+        score.classList.remove("off")
+        score.classList.add("on") 
+        questions.classList.remove("on")
+        questions.classList.add("off") 
+    }
 }
 // fetching()
 // setTimeout(() => {
@@ -79,13 +95,32 @@ form.addEventListener("submit", function(e){
     e.target.reset()
     amount = val.number
     URLs = `https://opentdb.com/api.php?amount=${val.number}&category=${val.catagory}&difficulty=${val.difficulty}&type=${val.type}`
-    sessionStorage.setItem("URL", URLs);
     fetching()
     console.log(questionArray)
+    // console.log(amount)
 
     setTimeout(()=>{
+        setup.classList.remove("on")
+        setup.classList.add("off")
+        questions.classList.remove("off")
+        questions.classList.add("on")
         render()
     }, 2000)
    
 })
 
+//restart
+const restart = document.querySelector(".restart");
+
+restart.addEventListener("click", () => {
+    questionArray = [];
+    i=0;
+    finalScore=0;
+    document.querySelector(".score-val").innerHTML=""
+    setup.classList.remove("off")
+    setup.classList.add("on")
+    score.classList.remove("on")
+    score.classList.add("off") 
+    questions.classList.remove("on")
+    questions.classList.add("off") 
+})
